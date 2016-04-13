53af9fd11a3f97a0d0077c88df3fb8fd60e3488d
joeysab <josephsabbagh@live.com>
2016-04-13 17:58:13 +0000

    Question 2 & 3 done

--- answers.js
+++ answers.js
@@ -1,7 +1,23 @@
+
+//Question 1
 function firstLetter (word){
-    var letter = word.chartAt(0);
+    var letter = word.charAt(0);
     return letter;
 }
 
-firstLetter('Hey');
+console.log(firstLetter('Hey'));
 
+//Question 2
+function lastLetter (word){
+    var letter = word.charAt(word.length - 1);
+    return letter
+}
+
+console.log(lastLetter("Joe"))
+
+//Question 3
+function numPosition (string, num){
+    var charAtposition = string.charAt(num)
+    return charAtposition;
+}
+console.log(numPosition("Salut",2))
