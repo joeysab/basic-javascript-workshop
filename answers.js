5491d9b42ca2a178c8833ea2b9ca8907e8c6d82a
joeysab <josephsabbagh@live.com>
2016-04-13 18:04:14 +0000

    Question 4 Done

--- answers.js
+++ answers.js
@@ -7,13 +7,14 @@ function firstLetter (word){
 
 console.log(firstLetter('Hey'));
 
+
 //Question 2
 function lastLetter (word){
     var letter = word.charAt(word.length - 1);
     return letter
 }
-
 console.log(lastLetter("Joe"))
+
 
 //Question 3
 function numPosition (string, num){
@@ -21,3 +22,11 @@ function numPosition (string, num){
     return charAtposition;
 }
 console.log(numPosition("Salut",2))
+
+
+//Question 4 -- if other than number type added function will concat both items
+function addition (num1, num2){
+    var total = num1 + num2;
+    return total
+}
+console.log(addition(2,3))
