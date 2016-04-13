0b306d7bb5cc483ea28111ea0bbc64d1ea8937a6
joeysab <josephsabbagh@live.com>
2016-04-13 19:01:25 +0000

    Question 8 Done

--- answers.js
+++ answers.js
@@ -5,7 +5,7 @@ function firstLetter (word){
     return letter;
 }
 
-console.log(firstLetter('Hey'));
+// console.log(firstLetter('Hey'));
 
 
 //Question 2
@@ -13,7 +13,7 @@ function lastLetter (word){
     var letter = word.charAt(word.length - 1);
     return letter
 }
-console.log(lastLetter("Joe"))
+// console.log(lastLetter("Joe"))
 
 
 //Question 3
@@ -21,7 +21,7 @@ function numPosition (string, num){
     var charAtposition = string.charAt(num)
     return charAtposition;
 }
-console.log(numPosition("Salut",2))
+// console.log(numPosition("Salut",2))
 
 
 //Question 4 -- if other than number type added function will concat both items
@@ -29,7 +29,7 @@ function addition (num1, num2){
     var total = num1 + num2;
     return total
 }
-console.log(addition(2,3))
+// console.log(addition(2,3))
 
 
 //Question 5 -- if input entered is not a number, function will return NaN
@@ -37,7 +37,7 @@ function multiplication (num1, num2) {
     var answer = num1 * num2;
     return answer;
 }
-console.log(multiplication(2,5))
+// console.log(multiplication(2,5))
 
 
 //Question 6
@@ -65,6 +65,14 @@ function wordRepeat(word,numOfRepeat){
     for (var i = 0; i< numOfRepeat; i++){
         console.log(word)
     }
-    return word.repeat(numOfRepeat);
 }
 wordRepeat("hey",4)
+
+
+//Question 8
+function invertedString(word){
+    var invert = word.split('').reverse();
+    var joinAgain = invert.join("");
+    return joinAgain;
+}
+//console.log(invertedString("Joseph"));
